b800b4f1128780cd44a9a48c37fe275076c97d65
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-06-11 01:46:05 -0600

    feat: add authenticated user and cart data fetching with axios

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,34 +1,55 @@
-'use client'
-import { productsDummyData, userDummyData } from "@/assets/assets";
-import { useUser } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { createContext, useContext, useEffect, useState } from "react";
+'use client';
+import { productsDummyData, userDummyData } from '@/assets/assets';
+import { useAuth, useUser } from '@clerk/nextjs';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { createContext, useContext, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
 export const AppContext = createContext();
 
 export const useAppContext = () => {
-    return useContext(AppContext)
-}
+    return useContext(AppContext);
+};
 
 export const AppContextProvider = (props) => {
 
-    const currency = process.env.NEXT_PUBLIC_CURRENCY
-    const router = useRouter()
+    const currency = process.env.NEXT_PUBLIC_CURRENCY;
+    const router = useRouter();
 
-    const { user} = useUser();
+    const { user } = useUser();
+    const { getToken } = useAuth();
 
-    const [products, setProducts] = useState([])
-    const [userData, setUserData] = useState(false)
-    const [isSeller, setIsSeller] = useState(true)
-    const [cartItems, setCartItems] = useState({})
+    const [products, setProducts] = useState([]);
+    const [userData, setUserData] = useState(false);
+    const [isSeller, setIsSeller] = useState(false);
+    const [cartItems, setCartItems] = useState({});
 
     const fetchProductData = async () => {
-        setProducts(productsDummyData)
-    }
+        setProducts(productsDummyData);
+    };
 
     const fetchUserData = async () => {
-        setUserData(userDummyData)
-    }
+        try {
+            if (user.publicMetadata.role == 'seller') {
+                setIsSeller(true);
+            }
+
+            const token = await getToken();
+
+            const { data } = await axios.get('/api/user/data', { headers: { Authorization: `Bearer ${token}` } });
+
+            if (data.success) {
+                setUserData(data.user);
+                setCartItems(data.user.cartItems);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+            toast.error(error.message);
+        }
+    };
 
     const addToCart = async (itemId) => {
 
@@ -41,7 +62,7 @@ export const AppContextProvider = (props) => {
         }
         setCartItems(cartData);
 
-    }
+    };
 
     const updateCartQuantity = async (itemId, quantity) => {
 
@@ -51,9 +72,9 @@ export const AppContextProvider = (props) => {
         } else {
             cartData[itemId] = quantity;
         }
-        setCartItems(cartData)
+        setCartItems(cartData);
 
-    }
+    };
 
     const getCartCount = () => {
         let totalCount = 0;
@@ -63,7 +84,7 @@ export const AppContextProvider = (props) => {
             }
         }
         return totalCount;
-    }
+    };
 
     const getCartAmount = () => {
         let totalAmount = 0;
@@ -74,18 +95,20 @@ export const AppContextProvider = (props) => {
             }
         }
         return Math.floor(totalAmount * 100) / 100;
-    }
+    };
 
     useEffect(() => {
-        fetchProductData()
-    }, [])
+        fetchProductData();
+    }, []);
 
     useEffect(() => {
-        fetchUserData()
-    }, [])
+        if (user) {
+            fetchUserData();
+        }
+    }, [user]);
 
     const value = {
-        user,
+        user, getToken,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
@@ -93,11 +116,11 @@ export const AppContextProvider = (props) => {
         cartItems, setCartItems,
         addToCart, updateCartQuantity,
         getCartCount, getCartAmount
-    }
+    };
 
     return (
         <AppContext.Provider value={value}>
             {props.children}
         </AppContext.Provider>
-    )
-}
+    );
+};
