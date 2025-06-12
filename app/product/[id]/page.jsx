5d736f09cdabfa11d5bb5b17ef476ec3434e6444
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-06-12 00:50:08 -0600

    fix: correct images field usage and update formatting in product detail page

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -1,32 +1,34 @@
-"use client"
-import { useEffect, useState } from "react";
-import { assets } from "@/assets/assets";
-import ProductCard from "@/components/ProductCard";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import Image from "next/image";
-import { useParams } from "next/navigation";
-import Loading from "@/components/Loading";
-import { useAppContext } from "@/context/AppContext";
-import React from "react";
+'use client';
+import { useEffect, useState } from 'react';
+import { assets } from '@/assets/assets';
+import ProductCard from '@/components/ProductCard';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import Image from 'next/image';
+import { useParams } from 'next/navigation';
+import Loading from '@/components/Loading';
+import { useAppContext } from '@/context/AppContext';
+import React from 'react';
 
 const Product = () => {
 
     const { id } = useParams();
 
-    const { products, router, addToCart } = useAppContext()
+    const { products, router, addToCart } = useAppContext();
 
     const [mainImage, setMainImage] = useState(null);
     const [productData, setProductData] = useState(null);
 
     const fetchProductData = async () => {
+        
+
         const product = products.find(product => product._id === id);
         setProductData(product);
-    }
+    };
 
     useEffect(() => {
         fetchProductData();
-    }, [id, products.length])
+    }, [id, products.length]);
 
     return productData ? (<>
         <Navbar />
@@ -35,7 +37,7 @@ const Product = () => {
                 <div className="px-5 lg:px-16 xl:px-20">
                     <div className="rounded-lg overflow-hidden bg-gray-500/10 mb-4">
                         <Image
-                            src={mainImage || productData.image[0]}
+                            src={mainImage || productData.images[0]}
                             alt="alt"
                             className="w-full h-auto object-cover mix-blend-multiply"
                             width={1280}
@@ -44,7 +46,7 @@ const Product = () => {
                     </div>
 
                     <div className="grid grid-cols-4 gap-4">
-                        {productData.image.map((image, index) => (
+                        {productData.images.map((image, index) => (
                             <div
                                 key={index}
                                 onClick={() => setMainImage(image)}
@@ -116,7 +118,7 @@ const Product = () => {
                         <button onClick={() => addToCart(productData._id)} className="w-full py-3.5 bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition">
                             Add to Cart
                         </button>
-                        <button onClick={() => { addToCart(productData._id); router.push('/cart') }} className="w-full py-3.5 bg-orange-500 text-white hover:bg-orange-600 transition">
+                        <button onClick={() => { addToCart(productData._id); router.push('/cart'); }} className="w-full py-3.5 bg-orange-500 text-white hover:bg-orange-600 transition">
                             Buy now
                         </button>
                     </div>
@@ -137,7 +139,7 @@ const Product = () => {
         </div>
         <Footer />
     </>
-    ) : <Loading />
+    ) : <Loading />;
 };
 
 export default Product;
