8996d1409c0d5c67421bdf22a69b7f1d7b23b340
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-06-12 23:47:30 -0600

    feat: add createUserOrder inngest function for batch order processing

--- config/inngest.js
+++ config/inngest.js
@@ -1,10 +1,10 @@
-import { Inngest } from "inngest";
-import { connect } from "mongoose";
-import connectDB from "./db";
-import User from "@/models/User";
+import { Inngest } from 'inngest';
+import connectDB from './db';
+import User from '@/models/User';
+import Order from '@/models/Order';
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "quickcart" });
+export const inngest = new Inngest({ id: 'quickcart' });
 
 // Inngest Function to save user data to a database
 export const syncUserCreation = inngest.createFunction(
@@ -13,17 +13,17 @@ export const syncUserCreation = inngest.createFunction(
     },
     { event: 'clerk/user.created' },
     async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
         const userData = {
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             imageUrl: image_url
-        }
-        await connectDB()
-        await User.create(userData)
+        };
+        await connectDB();
+        await User.create(userData);
     }
-)
+);
 
 // Inngest Function to update user data in the database
 export const syncUserUpdation = inngest.createFunction(
@@ -31,18 +31,18 @@ export const syncUserUpdation = inngest.createFunction(
         id: 'update-user-from-clerk'
     },
     { event: 'clerk/user.updated' },
-    async ({ }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
         const userData = {
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             imageUrl: image_url
-        }
-        await connectDB()
-        await User.findByIdAndUpdate(id, userData)
+        };
+        await connectDB();
+        await User.findByIdAndUpdate(id, userData);
     }
-)
+);
 
 // Inngest Function to delete user from database
 export const syncUserDeletion = inngest.createFunction(
@@ -51,9 +51,37 @@ export const syncUserDeletion = inngest.createFunction(
     },
     { event: 'clerk/user.deleted'},
     async ({event}) => {
-        const { id } = event.data
+        const { id } = event.data;
 
-        await connectDB()
-        await User.findByIdAndDelete(id)
+        await connectDB();
+        await User.findByIdAndDelete(id);
     }
-)
+);
+
+// Inngest Function to create user's order in the database
+export const createUserOrder = inngest.createFunction(
+    {
+        id:'create-user-order',
+        batchEvents: {
+            maxSize: 5,
+            timeout: '5s'
+        }
+    },
+    {event: 'order/create' },
+    async ({ events }) => {
+        const orders = events.map((event) => {
+            return {
+                userId: event.data.userId,
+                items: event.data.items,
+                amount: event.data.amount,
+                address: event.data.address,
+                date: event.data.date
+            };
+        });
+
+        await connectDB();
+        await Order.insertMany(orders);
+
+        return { success: true, processed: orders.length };
+    }
+);
