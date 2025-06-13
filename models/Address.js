42a54eb0f18582f39dfcbcd1b2908376d9f02fd8
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-06-12 23:48:17 -0600

    fix: update postalCode type to String and add user ref to address schema

--- models/Address.js
+++ models/Address.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
 const addressSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
+    userId: { type: String, required: true , ref: 'user' }, 
     fullName: { type: String, required: true },
     phoneNumber: { type: String, required: true },
-    postalCode: { type: Number, required: true },
+    postalCode: { type: String, required: true },
     neighborhood: { type: String, required: true },
     city: { type: String, required: true },
     state: { type: String, required: true },
