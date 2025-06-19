b5ccb5bc15531440f11039907fedb9e8b2f1b932
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-06-19 00:21:01 -0600

    feat: add order creation endpoint with amount calculation and user auth

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,26 +1,31 @@
 import connectDB from '@/config/db';
 import { inngest } from '@/config/inngest';
 import Product from '@/models/Product';
-import { getAuth, User } from '@clerk/nextjs/server';
+import User from '@/models/User';
+import { getAuth } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
+
 
 export async function POST(request) {
     try {
-        const { userId } = getAuth();
+        const { userId } = getAuth(request);
         const { address, items } = await request.json();
 
         if (!address || items.length === 0) {
             return NextResponse.json({ success: false, message: 'Address and items are required' }, { status: 400 });
         }
 
+        await connectDB();
+
         const amount = await items.reduce(async (acc, item) => {
             const product = await Product.findById(item.product);
-            return acc + product.offerPrice * item.quantity;
+            return await acc + product.offerPrice * item.quantity;
         }, 0);
 
         await inngest.send({
             name: 'order/create',
             data: {
+                userId,
                 address,
                 items,
                 amount: amount + Math.floor(amount * 0.02),
@@ -35,6 +40,8 @@ export async function POST(request) {
 
         return NextResponse.json({ success: true, message: 'Order placed' }, { status: 201 });
     } catch (error) {
+        console.error('Error creating order:', error.message);
+        // Log the error to Inngest or any other logging service if needed
         return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
     }
 }
