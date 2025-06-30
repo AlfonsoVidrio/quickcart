fab77839214fe80e13f7783a8de8fa2dfb410c32
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-06-30 05:36:03 -0600

    feat: add Stripe webhook API to handle payment intent events and update order status

---
+++ app/api/stripe/route.js
@@ -0,0 +1,56 @@
+import connectDB from '@/config/db';
+import Order from '@/models/Order';
+import User from '@/models/User';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export async function POST(request) {
+    try {
+        const body = await request.text();
+        const sig = request.headers.get('stripe-signature');
+
+        const event = stripe.webhooks.constructEvent(body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+
+        const handlePaymentIntent = async (paymentIntentId, isPaid) => {
+            const session = await stripe.checkout.sessions.list({
+                payment_intent: paymentIntentId
+            });
+
+            const { orderId, userId } = session.data[0].metadata;
+
+            await connectDB();
+
+            if (isPaid) {
+                await Order.findByIdAndUpdate(orderId, {isPaid: true});
+                await User.findByIdAndUpdate(userId, {cartItems: {}});
+            } else {
+                await Order.findByIdAndDelete(orderId);
+            }
+        };
+
+        switch (event.type) {
+            case 'payment_intent.succeeded':{
+                await  handlePaymentIntent(event.data.object.id, true);
+                break;
+            }
+            case 'payment_intent.canceled':{
+                await handlePaymentIntent(event.data.object.id, false);
+                break;
+            }
+            default:
+                console.warn(`Unhandled event type ${event.type}`);
+                break;
+        }
+
+        return NextResponse.json({ received: true }, { status: 200 });
+
+    } catch (error) {
+        console.error('Error verifying webhook signature:', error);
+        return NextResponse.json({ message: error.message }, { status: 400 });
+    }
+}
+
+export const config = {
+    api: { bodyParser: false }
+};
