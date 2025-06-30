7095af19503321fd2a2474638f6cc544b55e5e1a
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-06-30 05:35:38 -0600

    feat: add Stripe payment API route with order creation and checkout session

---
+++ app/api/order/stripe/route.js
@@ -0,0 +1,79 @@
+import connectDB from '@/config/db';
+import Order from '@/models/Order';
+import { getAuth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+import Product from '@/models/Product';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export async function POST(request) {
+    try {
+        const { userId } = getAuth(request);
+        const { address, items } = await request.json();
+        const origin = request.headers.get('origin');
+
+        if (!address || items.length === 0) {
+            return NextResponse.json({ success: false, message: 'Address and items are required' }, { status: 400 });
+        }
+
+        await connectDB();
+
+        let productData = [];
+
+        let amount = await items.reduce(async (acc, item) => {
+            const product = await Product.findById(item.product);
+            productData.push({
+                name: product.name,
+                price: product.offerPrice,
+                quantity: item.quantity,
+            });
+            return await acc + product.offerPrice * item.quantity;
+        }, 0);
+
+        amount += Math.floor(amount * 0.036);
+
+        const order = await Order.create({
+            userId,
+            address,
+            items,
+            amount,
+            date: Date.now(),
+            paymentType: 'Stripe'
+        });
+
+        // create line items for Stripe
+        const line_items = productData.map(item => {
+            return {
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: item.name
+                    },
+                    unit_amount: item.price * 100,
+                },
+                quantity: item.quantity
+            };
+        });
+
+        // create session with Stripe
+        const session = await stripe.checkout.sessions.create({
+            line_items,
+            mode:'payment',
+            success_url: `${origin}/order-placed`,
+            cancel_url: `${origin}/cart`,
+            metadata: {
+                orderId:order._id.toString(),
+                userId
+            }
+        });
+
+        const url = session.url;
+
+        return NextResponse.json({ success: true, url }, { status: 200 });
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ success: false, message: error.message }, { status: 500 });
+    }
+}
